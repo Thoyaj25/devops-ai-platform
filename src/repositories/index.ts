94a87c6347c9ffde69476db96108134b945de5d9
Durga Prasad Pisupati Janakiramaiah <prasadpj509@gmail.com>
2026-07-09 06:59:44 +0000

    refactor: clean project architecture and remove duplicate repositories

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,7 +1,6 @@
 export { userRepository } from "./userRepository";
 export { projectRepository } from "./projectRepository";
 export { deploymentRepository } from "./deploymentRepository";
-export { auditLogRepository } from "./auditLogRepository";
 export { auditRepository } from "./auditRepository";
 export {
  deploymentJobRepository
