f8d4a3e9a83315a0457761440aa4b397ea5309ab
Durga Prasad Pisupati Janakiramaiah <prasadpj509@gmail.com>
2026-09-07 08:07:05 +0000

    Add Kubernetes and Terraform infrastructure

---
+++ terraform/eks.tf
@@ -0,0 +1,50 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "21.24.0"
+
+  name               = "${var.project_name}-${var.environment}-eks"
+  kubernetes_version = "1.33"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  addons = {
+    coredns = {}
+
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+
+    kube-proxy = {}
+
+    vpc-cni = {
+      before_compute = true
+    }
+  }
+
+  eks_managed_node_groups = {
+    general = {
+      name = "${var.project_name}-${var.environment}-ng"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
+
+      capacity_type = "ON_DEMAND"
+
+      subnet_ids = module.vpc.private_subnets
+    }
+  }
+
+  tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "terraform"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region for MarketSphere infrastructure"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Project name used for AWS resource naming"
+  type        = string
+  default     = "marketsphere"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
